de3283041a6647ced3d013a2e36d35e5ddfbca17
巴思 <basi.dwz@taobao.com>
2014-06-07 16:53:06 +0800

    add event support

--- controllers/weixin.js
+++ controllers/weixin.js
@@ -12,16 +12,32 @@ var config = require('../config');
 
 var token = config.weixin.token;
 
-exports.dispatch = wechat(token, function (req, res, next) {
-  var message = req.weixin;
-  var content = message.Content;
-  if (content === 'ping') {
-    return res.reply('pong');
-  }
+function sendGreeting(res) {
   res.reply([{
     title: '周宁奕&余晓瑞婚礼进行时',
     description: '点击查看详情',
     picurl: 'http://swtest.qiniudn.com/getimgdata.jpeg',
     url: config.serverHost + '/pages/upload'
   }]);
-});
+}
+
+exports.dispatch = wechat(token)
+// When message is a event.
+.event(function (message, req, res, next) {
+  if (message.Event === 'subscribe') {
+    return sendGreeting(res);
+  }
+  if (message.Event === 'unsubscribe') {
+    return res.reply('Bye!');
+  }
+  res.reply('尚未支持！');
+})
+// When message is text.
+.text(function (message, req, res, next) {
+  var content = message.Content;
+  if (content === 'ping') {
+    return res.reply('pong');
+  }
+  return sendGreeting(res);
+})
+.middlewarify();
